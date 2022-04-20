f4baaae84070adfde35a2dafd50e9b4d028a0af6
strange-tiger <jinolee9525@gmail.com>
2022-04-20 15:55:19 +0900

    Feat: Upload JH introduce

--- Garagitgit/Garagitgit/Garagitgit.cpp
+++ Garagitgit/Garagitgit/Garagitgit.cpp
@@ -10,4 +10,6 @@ int main()
     cout << "Hello!!! ^ㅡㅡㅡ^" << endl;
     std::cout << "Hello collaboration!\n";
     std::cout << "Hello Collaboration!\n";
+
+    std::cout << "안녕하세요. 저는 이진호입니다. 25살입니다. 취미는 맞춰보세요.\n";
 }
