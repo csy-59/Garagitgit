237e089e01d3de4c369458d4e3c41b18340a3c8e
strange-tiger <jinolee9525@gmail.com>
2022-04-20 16:00:53 +0900

    Fix: Lack of ;

--- Garagitgit/Garagitgit/Garagitgit.cpp
+++ Garagitgit/Garagitgit/Garagitgit.cpp
@@ -6,7 +6,7 @@ int main()
 {
     std::cout << "Hello Colllaboration\n";
     std::cout << "My name is Jinho Lee\n";
-}
+
     cout << "Hello!!! ^ㅡㅡㅡ^" << endl;
     std::cout << "Hello collaboration!\n";
     std::cout << "Hello Collaboration!\n";
